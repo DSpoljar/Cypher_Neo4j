df2a630cb597e205fe4ac64487d57b76b07d4107
Denis <40057681+DSpoljar@users.noreply.github.com>
2021-06-20 17:21:30 +0200

    Added simple IO template for upcoming purposes.

---
+++ Main/src/IOHandler.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IOHandler
+{
+
+    // Constructor
+    public IOHandler() throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String name = reader.readLine();
+
+
+        System.out.println(name);
+    }
+
+
+
+
+
+
+}
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,10 +1,13 @@
-
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("helloworld");
+        IOHandler handler = new IOHandler(); // Creating a simple IO machinery as base for upcoming tasks.
+
     }
 
 }
