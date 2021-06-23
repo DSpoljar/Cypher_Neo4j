2683217d82d924bf227335be40de0b07b24cd0e2
Denis <40057681+DSpoljar@users.noreply.github.com>
2021-06-23 16:20:46 +0200

    integrated ANTLR in Intellij and provided an example output tree

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,6 +2,10 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+
+
+
+
 public class Main
 {
     public static void main(String[] args) throws IOException
