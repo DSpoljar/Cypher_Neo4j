66ffc68ed5f2fb29cca200b63bc8bf08d8be832a
Denis <40057681+DSpoljar@users.noreply.github.com>
2021-07-04 13:38:03 +0200

    Added graphical reprsentation and generated sample output

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,6 +2,7 @@ import Cypher_Files.*;
 import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenSource;
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -10,9 +11,9 @@ import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
-
-
+import javax.swing.*;
 import java.io.*;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 
@@ -24,10 +25,12 @@ public class Main
         IOHandler handler = new IOHandler(); // Creating a simple IO machinery as base for upcoming tasks.
 
         String query = handler.inputReader();
+
+        // This may be used in case we'll work with inputs over files.
         // InputStream inptStream = new FileInputStream(query);
 
         CharStream test = (CharStream) CharStreams.fromString(query);
-        // ANTLRInputStream in = new ANTLRInputStream(inptStream);
+        // ANTLRInputStream in = new ANTLRInputStream(inptStream); <- Deprecated?
 
         CypherLexer cyphLexer = new CypherLexer(test);
 
@@ -35,17 +38,24 @@ public class Main
 
         CypherParser cyphPars = new CypherParser(tokenStream);
 
-        cyphPars.oC_Query();
+        // cyphPars.oC_Query();
 
-        /*
-        String query2 =
-        "MATCH (n)" + "\n" +
-        "WHERE id(n) = $id" + "\n" +
-           "RETURN n.name";
-         */
+        ParseTree tree = cyphPars.oC_Query();
+
+        // Output tree in pure String format.
+        System.out.println(tree.toStringTree(cyphPars));
 
 
+        // JFrame and Panel for visualization.
 
+        JFrame frame = new JFrame("Testing the tree ...");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(cyphPars.getRuleNames()), tree);
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
 
 
 
